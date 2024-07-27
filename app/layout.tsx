705ee8d6e67c2369a7e352f829c8682183c4ab63
Misho Shubitidze <misho@mshub.dev>
2024-07-27 12:14:42 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { TailwindIndicator } from "./_components/tailwind-indicator";
-import { SiteHeader } from "./_components/site-header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <SiteHeader />
         {children}
         <TailwindIndicator />
       </body>
